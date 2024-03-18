69238488c1d4de0de644be91c3e943d66fc57f14
Alex Katrenko <112182502+codemorphist@users.noreply.github.com>
2024-03-18 19:00:59 +0200

    Updated test.py

--- chess/test.py
+++ chess/test.py
@@ -3,7 +3,7 @@ from engine import find_mat_two
 from graphic import draw_board
 
 
-fen = "r2qkb1r/pp2nppp/3p4/2pNN1B1/2BnP3/3P4/PPP2PPP/R2bK2R w KQkq - 1 0"
+fen = input()
 chess = Chess(fen)
 draw_board(chess)
 
