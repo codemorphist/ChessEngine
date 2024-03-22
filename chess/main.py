0a0a49d588c6bc2761f9a674a86012699c88b7a5
Alex Katrenko <112182502+codemorphist@users.noreply.github.com>
2024-03-22 17:04:54 +0200

    Improved main.py with time elapsed

--- chess/main.py
+++ chess/main.py
@@ -2,12 +2,20 @@ from chess import *
 from engine import find_mat_two
 from graphic import draw_board
 
+import time
+from datetime import timedelta
 
-fen = input()
+
+fen = input("Input FEN of chess position: ")
 chess = Chess(fen)
 draw_board(chess)
+
+start = time.time()
 
 for mat in find_mat_two(fen):
     for m in mat:
         print(m)
     print(10 * "-")
+
+end = time.time()
+print("Elapsed time:", timedelta(seconds=end-start))
